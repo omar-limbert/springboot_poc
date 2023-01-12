cf27a858e9c51ea87d855a68781889d4581d90fb
Omar Huanca <omar.huanca@agilifyus.com>
2023-01-12 18:52:35 -0400

    added POST/PUT/DELETE Methods and docs

--- src/main/java/com/springboot/poc/demo/core/controllers/UserController.java
+++ src/main/java/com/springboot/poc/demo/core/controllers/UserController.java
@@ -3,11 +3,8 @@ package com.springboot.poc.demo.core.controllers;
 import com.springboot.poc.demo.core.entities.User;
 import com.springboot.poc.demo.core.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -16,13 +13,28 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping("/users")
+    @RequestMapping(method = RequestMethod.GET, value = "/users")
     public List<User> getAllUsers() {
         return userService.getUserList();
     }
 
-    @RequestMapping("/users/{userName}")
+    @RequestMapping(method = RequestMethod.GET, value = "/users/{userName}")
     public User getUserByName(@PathVariable String userName) {
         return userService.getUserByName(userName);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/users")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/users/{userName}")
+    public void updateUser(@RequestBody User user, @PathVariable String userName) {
+        userService.updateUserByName(userName, user);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/users/{userName}")
+    public void deleteUser(@PathVariable String userName) {
+        userService.deleteUserByName(userName);
     }
 }
--- src/main/java/com/springboot/poc/demo/core/services/UserService.java
+++ src/main/java/com/springboot/poc/demo/core/services/UserService.java
@@ -4,13 +4,15 @@ import com.springboot.poc.demo.core.entities.User;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class UserService {
 
     @Getter
-    private final List<User> userList = List.of(
+    private List<User> userList = new ArrayList<>(Arrays.asList(
             new User("User 1", "18", "M"),
             new User("User 2", "28", "M"),
             new User("User 3", "38", "F"),
@@ -19,9 +21,30 @@ public class UserService {
             new User("User 6", "48", "F"),
             new User("User 7", "48", "F"),
             new User("User 8", "48", "F")
-    );
+    ));
 
     public User getUserByName(String userName) {
-        return userList.stream().filter(t -> t.getName().equalsIgnoreCase(userName)).findFirst().get();
+        return userList.stream()
+                .filter(t -> t.getName().equalsIgnoreCase(userName))
+                .findFirst()
+                .get();
+    }
+
+    public void addUser(User user) {
+        userList.add(user);
+    }
+
+    public void updateUserByName(String userName, User user) {
+        for (int index = 0; index < userList.size(); index++) {
+            User userToCheck = userList.get(index);
+            if (userToCheck.getName().equalsIgnoreCase(userName)) {
+                userList.set(index, user);
+                break;
+            }
+        }
+    }
+
+    public void deleteUserByName(String userName) {
+        userList.removeIf(user -> user.getName().equalsIgnoreCase(userName));
     }
 }
